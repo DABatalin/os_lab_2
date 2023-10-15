5ab2c4be4110974ac67476f8b18f1cb377ab68e4
DABatalin <mityadb@yandex.ru>
2023-10-15 23:30:32 +0300

    add tests; change code; add doc

--- src/main.c
+++ src/main.c
@@ -2,8 +2,11 @@
 #include "stdlib.h" 	// pid_t declaration is here
 #include "unistd.h" 	// fork() and getpid() declarations are here
 #include "pthread.h"
-#include "time.h"       // for clock_t, clock(), CLOCKS_PER_SEC
+#include <sys/time.h>     // for clock_t, clock(), CLOCKS_PER_SEC
 #include <string.h>
+
+pthread_mutex_t player1_mutex;
+pthread_mutex_t player2_mutex;
 
 typedef struct
 {
@@ -14,46 +17,53 @@ typedef struct
 	int player2_start_points;
     int player1_wins;
 	int player2_wins;
-    pthread_mutex_t player1_mutex;
-    pthread_mutex_t player2_mutex;
 } game_parameters;
 
 
-void* experiment(game_parameters *game) {
+void* experiment(void *param) {
+	game_parameters *game = (game_parameters*) param;
+
+	int player1_wins = 0;
+	int player2_wins = 0;
 	// printf("experiment_num: %d", game->experiment_num);
 	for (int j = 0; j < game->experiment_num; j++) {
 		int player1_experiment_points = game->player1_start_points;
 		int player2_experiment_points = game->player2_start_points;
-		// srand(game->throws_number);
 
 		for (int i = game->tour_number; i < game->throws_number; i++) {
-			player1_experiment_points += rand() % 6 + 1 + rand() % 6 + 1;
-			player2_experiment_points += rand() % 6 + 1 + rand() % 6 + 1;
+			player1_experiment_points += rand() % 12 + 1;
+			player2_experiment_points += rand() % 12 + 1;
 		}
-		// printf("player1_experiment_points: %d\n", player1_experiment_points);
-		// printf("player2_experiment_points: %d\n", player2_experiment_points);
-
 		if (player1_experiment_points != player2_experiment_points) {
 			if (player1_experiment_points > player2_experiment_points) {
-				pthread_mutex_lock(&game->player1_mutex);
-				game->player1_wins += 1;
-				pthread_mutex_unlock(&game->player1_mutex);
+				player1_wins += 1;
 			}
 			else {
-				pthread_mutex_lock(&game->player2_mutex);
-				game->player2_wins += 1;
-				pthread_mutex_unlock(&game->player2_mutex);
+				player2_wins += 1;
 			}
 		}
 	}
+	// printf("PLayer1 wins: %d\n", player1_wins);
+	// printf("PLayer2 wins: %d\n\n", player2_wins);
+	pthread_mutex_lock(&player1_mutex);
+	game->player1_wins += player1_wins;
+	pthread_mutex_unlock(&player1_mutex);
+
+	pthread_mutex_lock(&player2_mutex);
+	game->player2_wins += player2_wins;
+	pthread_mutex_unlock(&player2_mutex);
+	
+	pthread_exit(0);
 }
 
 
 int main(int argc, char **argv) {
+	srand(time(NULL));
 	int threads_number = 1;
 	if ((argc > 3) || (argc == 2)) {
 		perror("Wrong number of parameters");
-		return 1;
+		printf("You wrote %d parametres, but it must be only 2 or 0", argc);
+		return 2;
     }
 
 	// for (int i = 0; i < argc; i++) {
@@ -66,36 +76,40 @@ int main(int argc, char **argv) {
 		}
 		else {
 			perror("No such parameter exists");
-			return 2;
+			return 3;
 		}
 	}
 	
-	double time_spent = 0.0;
 	int experiment_number = 1;
 	game_parameters game = {0};
 
-	pthread_mutex_init(&game.player1_mutex, NULL);
-	pthread_mutex_init(&game.player2_mutex, NULL);
+
+	pthread_mutex_init(&player1_mutex, NULL);
+	pthread_mutex_init(&player2_mutex, NULL);
 
 	printf("Enter the number of throws (K-number): ");
 	scanf("%d", &game.throws_number);
-	printf("Enter the tour number: ");
+	printf("\nEnter the tour number: ");
 	scanf("%d", &game.tour_number);
-	printf("Enter the amount of points of the first player: ");
+	printf("\nEnter the amount of points of the first player: ");
 	scanf("%d", &game.player1_start_points);
-	printf("Enter the amount of points of the second player: ");
+	printf("\nEnter the amount of points of the second player: ");
 	scanf("%d", &game.player2_start_points);
-	printf("Enter the number of experiments: ");
+	printf("\nEnter the number of experiments: ");
 	scanf("%d", &experiment_number);
 
 	if (experiment_number < threads_number) {
 		perror("The number of threads is more than number of experiments");
-		return 3;
+		return 4;
 	}
 	game.experiment_num = experiment_number / threads_number;
-
 	pthread_t tid[threads_number];
-	clock_t begin = clock();
+
+
+    struct timeval start_time, end_time;
+    long seconds, microseconds;
+    gettimeofday(&start_time, NULL);
+
     for (int i=0; i<threads_number; i++)
     {
         pthread_create(&tid[i], NULL, experiment, &game);
@@ -105,15 +119,18 @@ int main(int argc, char **argv) {
     {
         pthread_join(tid[i], NULL);
     }
-	clock_t end = clock();
+
+	gettimeofday(&end_time, NULL);
+    seconds = end_time.tv_sec - start_time.tv_sec;
+    microseconds = end_time.tv_usec - start_time.tv_usec;
+    double elapsed_time = seconds + microseconds / 1e6;
+
 	double player1_chance = (double)game.player1_wins / (double)(game.player1_wins + game.player2_wins);
 	double player2_chance = (double)game.player2_wins / (double)(game.player1_wins + game.player2_wins);
-	printf("First player chance to win: %f\n", player1_chance);
+
+	printf("\nFirst player chance to win: %f\n", player1_chance);
 	printf("Second player chance to win: %f\n", player2_chance);
+    printf("The elapsed time is %f seconds\n", elapsed_time);
 
-	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
- 
-    printf("The elapsed time is %f seconds\n", time_spent);
 	return 0;
-
 }
